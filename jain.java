86472e5ac912dbd31a7d4af07bf52e1b9cb38639
AbhishekIreddy <129049366+AbhishekIreddy@users.noreply.github.com>
2023-08-10 09:44:10 +0530

    Update jain.java

--- jain.java
+++ jain.java
@@ -1,5 +1,7 @@
 class jain {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("Happy Death Day");
+        
     }
 }
